73845abb00f6dc4597b3a405edb6860447f45138
wujiaqi <jw5@student.unimelb.edu.au>
2021-04-23 17:49:50 +1000

    Update util.py

--- IG/util.py
+++ IG/util.py
@@ -2,11 +2,75 @@ from IG.search_algorithms import *
 import random
 
 
+<<<<<<< Updated upstream
 # move token from start to end 
 def move_token(symbol_type, start, end, play_dict):
     # move the token by adding new position
     play_dict["player"][symbol_type].remove(start)
     
+=======
+# return number of nodes count in player_class
+# return:
+#       {"s": 4, "r":2, "p":1}      #4 Scissors, 2 rock, 1 paper
+def get_current_player_nodes_count(player_class, player_side):
+    count_dict = {}
+    count_dict["r"] = len(player_class.play_dict[player_side]["r"])
+    count_dict["p"] = len(player_class.play_dict[player_side]["p"])
+    count_dict["s"] = len(player_class.play_dict[player_side]["s"])
+    return count_dict
+
+
+# params:
+#           cur_point = (x_coordinate, y_coordinate)
+# check if a point is not off the map)
+def is_point_on_map(play_dict, cur_point):
+    #check if off the map
+    if abs(cur_point[0]) > 4 or abs(cur_point[1]) > 4:
+        return False
+    if abs(cur_point[0] + cur_point[1]) > 4:
+        return False
+    else:
+        return True
+
+#return a string for point's symbol given a location, two together means it has multiple symbol at that point
+def get_symbol_by_location(whichplayer,play_dict,point):
+    out = ""
+    if point in find_player_symbols(play_dict, whichplayer, "r"):
+        out += "r"
+    if point in find_player_symbols(play_dict, whichplayer, "p"):
+        out += "p"
+    if point in find_player_symbols(play_dict, whichplayer, "s"):
+        out += "s"
+    return out
+
+# Remove all symbols of that type at that location
+def remove_all_type_symbols_at_location(player_class, removetype, location):
+    player_class.play_dict["player"][removetype] = \
+                    list(filter(lambda a: a != location, player_class.play_dict["player"][removetype]))
+    player_class.play_dict["opponent"][removetype] = \
+                    list(filter(lambda a: a != location, player_class.play_dict["opponent"][removetype]))
+
+
+""" -------------------- PLAYER ACTION ---------------------"""
+# Throw Action, reduce current throw number
+def action_throw(symbol_type, point):
+    return ("THROW",symbol_type, point)
+# Slide Action
+def action_slide(start, end):
+    return ("SLIDE",start, end)
+# Swing Action
+def action_swing(start, end):
+    return ("SWING",start, end)
+
+#Initialize throw range, player: "lower" or "upper"
+def Init_throw_range(player_class):
+    if (player_class.side == "upper"):
+        player_class.throw_range = range(4,5)
+        player_class.side = "upper"
+    else:
+        player_class.throw_range = range(-4, -3)
+        player_class.side = "lower"
+>>>>>>> Stashed changes
 
 # If two symbols place together, determine what to do with board_dict
 def eliminate_and_update_board(play_dict, target_dict):
